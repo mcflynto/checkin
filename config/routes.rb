412e7a2054d91df6de4ef569ac2f7a4a61f9a7e9
Julian Treutler <julian.treutler@9elements.com>
2020-12-02 16:49:45 +0100

    Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root :to => 'users#index'
   resources :users
+  resources :user_sessions, only: [:new, :create, :destroy]
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end
